4b03a4a71fb32385a0842ce582e8813617d0875a
Nico Pradas <118016471+Nicopradas@users.noreply.github.com>
2025-01-04 20:36:42 +0100

    create 16x16 grid

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ for (i=1;i<=16;i++){
     container.appendChild(horizontalSquare);
     for (f=1;f<=16;f++){
         const verticalSquare = document.createElement('div');
-        verticalSquare.textContent='*';
+        verticalSquare.textContent='';
+        verticalSquare.style.height='25px';
+        verticalSquare.style.width='25px';
+        verticalSquare.style.border='0.5px solid black';
         horizontalSquare.appendChild(verticalSquare);
     }
     
